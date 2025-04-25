56e2425d79194503ac86337bbb4044f484276e92
Diego Morales <Diegoalef1311moralef@gmail.com>
2025-04-25 18:46:56 -0400

    documentacion y primeras implementaciones de mis  proyectos realizados al  portafolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { projects } from '../data/projects'
 
 export const Portfolio = () => {
+
+
+
   return (
     <div className='page'>
 
       <h1 className='heading'>Portafolio</h1>
+
+      {
+
+        projects.map(project => {
+          return (
+          <article className='projects-portfolio' key={project.id}>
+            <br />
+            <span>{project.category}</span>
+            <h2>{project.name}</h2>
+            <h3>{project.technologies}</h3>
+            <p>{project.description}</p>
+            <a href='/projects/{project.url}'>Documentacion</a>
+          </article>
+          )
+        })
+
+      }
 
     </div>
   )
--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -4,7 +4,50 @@ export const projects = [
 
         'id': '1',
         'name': 'Diego Morales WEB',
-        'url': ''
+        'url': 'https://mi-portafolio-blond-pi.vercel.app/home',
+        'technologies': 'HTML, CSS, JavaScript, ReactJs',
+        'category': 'Desarrollo web',
+        'description': 'Mi pagina para presentar mis desarrollos y proyectos ejecutado y por ejecutar.'
+
+    },
+    {
+
+        'id': '2',
+        'name': 'API REST para una red social',
+        'url': 'https://mi-portafolio-blond-pi.vercel.app/home',
+        'technologies': 'JavaScript, NodeJs, ExpressJs, MongoDB',
+        'category': 'Desarrollo de API Rest Backend',
+                'description': 'Mi pagina para presentar mis desarrollos y proyectos ejecutado y por ejecutar.'
+
+    },
+    {
+
+        'id': '3',
+        'name': 'Blog Personal',
+        'url': 'https://mi-portafolio-blond-pi.vercel.app/home',
+        'technologies': 'HTML, CSS, JavaScript, ReactJs, NodeJs, Expressjs, MongoDB',
+        'category': 'Desarrollo web',
+                'description': 'Mi pagina para presentar mis desarrollos y proyectos ejecutado y por ejecutar.'
+
+    },
+    {
+
+        'id': '4',
+        'name': 'Imitacion de Spotify',
+        'url': 'https://mi-portafolio-blond-pi.vercel.app/home',
+        'technologies': 'HTML, CSS, JavaScript, Booststrap',
+        'category': 'Desarrollo web basico',
+                'description': 'Mi pagina para presentar mis desarrollos y proyectos ejecutado y por ejecutar.'
+
+    },
+    {
+
+        'id': '5',
+        'name': 'Pokedex',
+        'url': 'https://mi-portafolio-blond-pi.vercel.app/home',
+        'technologies': 'HTML, CSS, JavaScript, Booststrap',
+        'category': 'Desarrollo web basico',
+                'description': 'Mi pagina para presentar mis desarrollos y proyectos ejecutado y por ejecutar.'
 
     }
 
